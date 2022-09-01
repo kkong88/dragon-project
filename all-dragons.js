5532204f1f44dd241cafe76c0ff1c12faef9a268
max <novarecon@yahoo.com>
2022-09-01 16:09:05 -0700

    Phase 1

--- all-dragons.js
+++ all-dragons.js
@@ -1,4 +1,23 @@
-// Your code here
+/*class Dragon{
+  constructor(name,color){
+    this.name = name
+    this.color = color
+  }
+  breathesFire(name){
+    return (`${this.name} breathes fire everywhere! BURN!!!!`)
+  }
+  static getDragons(...dragons){
+    dragons.map((dragon) => dragon.name)
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+*/
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,20 @@
-// Your code here
+class Dragon{
+  constructor(name,color){
+    this.name = name
+    this.color = color
+  }
+  breathesFire(name){
+    return (`${this.name} breathes fire everywhere! BURN!!!!`)
+  }
+  static getDragons(...dragons){
+   return  dragons.map((dragon) => dragon.name)
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
